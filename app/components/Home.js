e3df221d637edad8351fbc3d27274fdab8dafca1
Andre Schiemann <Andre Schiemann>
2016-05-04 17:22:40 +0100

    Launching rocket - 3.. 2.. 1..

---
+++ app/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className='alert alert-info'>
+        I am ze Home component bruh.
+      </div>
+    );
+  }
+}
+
+export default Home;
